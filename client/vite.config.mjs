5506dac4d20646df6f62063a4f72b232ac6a5b8f
Karthik V <vinodkarthik2005@gmail.com>
2026-05-10 10:27:45 +0530

    Changes to vite.config.mjs for Vercel Deploy

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/client/', // Updated base path for deployment in a subdirectory
   server: {
     port: 5173
   }
